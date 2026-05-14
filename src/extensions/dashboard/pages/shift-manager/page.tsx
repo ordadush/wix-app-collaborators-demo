9836ccd19cf673aa161d0245e47664fcd83d071b
Wix App Builder Remote Machine (31daa8a3-6c1b-42d8-bd51-de012105b162) <support@wix.com>
2026-05-14 12:06:15 +0000

    Auto-Save changes

--- src/extensions/dashboard/pages/shift-manager/page.tsx
+++ src/extensions/dashboard/pages/shift-manager/page.tsx
@@ -109,7 +109,7 @@ export default function ShiftManagerPage() {
     {
       title: "Employee Name",
       render: (row: Shift) => row.employeeName,
-      width: "40%",
+      width: "60%",
     },
     {
       title: "Date",
