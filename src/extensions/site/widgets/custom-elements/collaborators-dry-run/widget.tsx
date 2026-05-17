182c4ad972555acde62a088ea8add344fac73ebb
Wix App Builder Remote Machine (71b88059-d743-4714-b78f-93cc6af136d3) <support@wix.com>
2026-05-17 12:01:33 +0000

    Auto-Save changes

--- src/extensions/site/widgets/custom-elements/collaborators-dry-run/widget.tsx
+++ src/extensions/site/widgets/custom-elements/collaborators-dry-run/widget.tsx
@@ -18,7 +18,7 @@ const collaborators = [
 ];
 
 const CollaboratorsDryRun: FC<WidgetProps> = ({
-  title = 'YAY GREAT DEMO1',
+  title = 'YAY GREAT DEMO2',
   subtitle = 'Active team members',
   bgColor = '#0b2ce6ff',
   accentColor = '#6c63ff',
