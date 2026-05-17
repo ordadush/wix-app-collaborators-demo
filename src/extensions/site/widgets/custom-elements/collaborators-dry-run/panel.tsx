2bb8b011d28f57e7c1c87b7be2354a3e5d3fa313
Wix App Builder Remote Machine (71b88059-d743-4714-b78f-93cc6af136d3) <support@wix.com>
2026-05-17 12:38:46 +0000

    Auto-Save changes

--- src/extensions/site/widgets/custom-elements/collaborators-dry-run/panel.tsx
+++ src/extensions/site/widgets/custom-elements/collaborators-dry-run/panel.tsx
@@ -10,7 +10,7 @@ import '@wix/design-system/styles.global.css';
 import { inputs, widget } from '@wix/editor';
 import React, { type FC, useCallback, useEffect, useState } from 'react';
 
-const DEFAULT_BG = '#290505ff';
+const DEFAULT_BG = '#ff0000ff';
 const DEFAULT_ACCENT = '#ffffffff';
 const DEFAULT_TEXT = '#e8e6ff';
 
