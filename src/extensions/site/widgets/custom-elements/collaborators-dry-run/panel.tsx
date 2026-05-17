f3a274bc37a0b3d495c2314f2cf21ab9171fa931
Wix App Builder Remote Machine (31daa8a3-6c1b-42d8-bd51-de012105b162) <support@wix.com>
2026-05-17 12:17:23 +0000

    Auto-Save changes

--- src/extensions/site/widgets/custom-elements/collaborators-dry-run/panel.tsx
+++ src/extensions/site/widgets/custom-elements/collaborators-dry-run/panel.tsx
@@ -11,7 +11,7 @@ import { inputs, widget } from '@wix/editor';
 import React, { type FC, useCallback, useEffect, useState } from 'react';
 
 const DEFAULT_BG = '#290505ff';
-const DEFAULT_ACCENT = '#6c63ff';
+const DEFAULT_ACCENT = '#ffffffff';
 const DEFAULT_TEXT = '#e8e6ff';
 
 const ColorField: FC<{ label: string; value: string; onChange: (v: string) => void }> = ({ label, value, onChange }) => (
