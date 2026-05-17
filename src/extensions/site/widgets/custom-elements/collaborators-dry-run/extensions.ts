2d4c850401dfd26b9a8ec8607cf58a0405e07580
Wix App Builder Remote Machine (71b88059-d743-4714-b78f-93cc6af136d3) <support@wix.com>
2026-05-17 11:53:25 +0000

    Update custom element widget title to 'Sharon123'.

--- src/extensions/site/widgets/custom-elements/collaborators-dry-run/extensions.ts
+++ src/extensions/site/widgets/custom-elements/collaborators-dry-run/extensions.ts
@@ -2,7 +2,7 @@ import { extensions } from '@wix/astro/builders';
 
 export const customelementwidgetCollaboratorsDryRun = extensions.customElement({
   id: '1034d21f-bc0c-46e6-ba18-545dd15adda7',
-  name: 'Demo for collaborators',
+  name: 'Sharon123',
   tagName: 'collaborators-dry-run',
   element: './extensions/site/widgets/custom-elements/collaborators-dry-run/widget.tsx',
   settings: './extensions/site/widgets/custom-elements/collaborators-dry-run/panel.tsx',
