d17b0d2e53cc62f8668a6eda54fe117fc3572a9e
Wix App Builder Remote Machine (31daa8a3-6c1b-42d8-bd51-de012105b162) <support@wix.com>
2026-05-17 12:18:06 +0000

    Auto-Save changes

--- src/extensions/site/widgets/custom-elements/collaborators-dry-run/widget.tsx
+++ src/extensions/site/widgets/custom-elements/collaborators-dry-run/widget.tsx
@@ -27,7 +27,7 @@ const CollaboratorsDryRun: FC<WidgetProps> = ({
   const [activeIndex, setActiveIndex] = useState<number | null>(null);
   const [mounted, setMounted] = useState(false);
 
-  //asdsajdsadj
+  //asdsajds
 
   useEffect(() => {
     setMounted(true);
