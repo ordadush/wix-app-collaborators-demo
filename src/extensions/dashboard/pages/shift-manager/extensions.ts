3251b713820c5c3001d80eae96cdf0a04375992e
Wix App Builder Remote Machine (31daa8a3-6c1b-42d8-bd51-de012105b162) <support@wix.com>
2026-05-20 15:29:25 +0000

    Auto-Save changes

--- src/extensions/dashboard/pages/shift-manager/extensions.ts
+++ src/extensions/dashboard/pages/shift-manager/extensions.ts
@@ -3,7 +3,7 @@ import { extensions } from "@wix/astro/builders";
 export const dashboardpageShiftManager = extensions.dashboardPage({
   id: "dddf02cc-9054-4992-bf13-aa5486db9518",
   title: "Shift Manager",
-  routePath: "shift-manage",
+  routePath: "shift-manager",
   component: "./extensions/dashboard/pages/shift-manager/page.tsx",
   createdBy: {
     author: "CODE_GEN",
